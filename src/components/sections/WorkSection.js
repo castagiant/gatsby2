d944b6141e6a8bc0b7f5784ffa53e42094f7a463
castagiant <louismalatesta0@gmail.com>
2019-10-01 19:08:18 -0700

    Update WorkSection.js

--- src/components/sections/WorkSection.js
+++ src/components/sections/WorkSection.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import PropTyps from 'prop-types';
+import { render } from 'react-dom';
+import Gallery from 'react-grid-gallery';
 
-import pic06 from '../../images/pic06.jpg'
+import pic06 from '../../images/pic06.jpg';
+import americancommercial from '../../images/american-commercial.mp3';
 
-class WorkSection extends React.Component {
+
+const IMAGES =
+[{
+        src: "https://live.staticflickr.com/65535/48801824983_417fb43dbe_h.jpg", 
+        thumbnail: "https://live.staticflickr.com/65535/48801824983_417fb43dbe_h.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+        //isSelected: true,
+        //tags: [{value: "", title: ""},]
+        //caption: ""
+},
+{
+        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 212,
+        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+        caption: "Boats (Jeshu John - designerspics.com)"
+},
+ 
+{
+        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+}]
+
+
+
+
+class WorkSection extends React.Component {/*{
   constructor(...args) {
     super(...args)
     this.state = {
@@ -49,22 +83,36 @@ class WorkSection extends React.Component {
         ))}
       </div>
     )
-  }
+  } */
+
+
 
   render() {
-    const { selectedPictureIndex } = this.state
+   // const { selectedPictureIndex } = this.state
 
     return (
       <div>
-        {selectedPictureIndex >= 0
+        {/*{selectedPictureIndex >= 0
           ? this.renderSelectedPicture(selectedPictureIndex)
-          : this.renderPictureGrid()}
+        : this.renderPictureGrid()}*/}
+
+        <div>
+          
+        <Gallery images={IMAGES}/>
+        </div>
+
+        <div>
+          <audio ref="audio_tag" src={americancommercial} controls autoPlay/>
+        </div>
+
+
+
         <iframe
           width="560px"
           height="400px"
           src="https://www.youtube.com/embed/Co64Qnle2uQ?autoplay=1"
           frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" //autoplay
           allowfullscreen
         />
       </div>
